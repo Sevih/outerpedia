10069be6c39c24e5b82f0e791e6d7fcc63d8a357
sevih <sevihmiaou@gmail.com>
2025-07-10 10:34:16 +0200

    video AL ars nova

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 // /public/service-worker.js
 
-const CACHE_NAME = 'outerpedia-cache-v1.35.29';
+const CACHE_NAME = 'outerpedia-cache-v1.35.30';
 const urlsToCache = [
   '/',
   '/manifest.json',
--- src/app/guides/_contents/adventure-license/ars-novaAL.tsx
+++ src/app/guides/_contents/adventure-license/ars-novaAL.tsx
@@ -2,6 +2,7 @@
 
 import GuideHeading from '@/app/components/GuideHeading'
 import TeamTabSelector from '@/app/components/TeamTabSelector'
+import YoutubeEmbed from '@/app/components/YoutubeEmbed'
 
 const teams = {
   team1: {
@@ -33,10 +34,11 @@ export default function ArsNovaGuide() {
 
       <div className="mb-4">
         <h3 className="text-lg font-bold text-sky-300 border-l-4 border-sky-500 pl-3 mb-2 mt-6">Combat Footage</h3>
-        <p className="mb-2 text-neutral-300">
-          A sample video of this team comp will be added here soon.
+        <p className="text-neutral-400 text-sm italic mt-2">
+          Run provided by <span className="text-white font-semibold">Sevih</span> (10/07/2025)
         </p>
+        <YoutubeEmbed videoId="Gb-649eighM" title="Ars Nova - Adventure License - Stage 10 - 1 run clear" />
       </div>
-    </div>
+    </div >
   )
 }
