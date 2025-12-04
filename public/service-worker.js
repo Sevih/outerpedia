df3800c8cf2e0a7101473291c28aea8713aaa8b4
sevih <sevihmiaou@gmail.com>
2025-12-04 16:52:33 +0100

    monad 2-1 [no-discord]

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 // /public/service-worker.js
 
-const CACHE_NAME = 'outerpedia-cache-v8.3.40';
+const CACHE_NAME = 'outerpedia-cache-v8.3.41';
 const urlsToCache = [
   '/',
   '/manifest.json',
--- src/data/monad/routes.ts
+++ src/data/monad/routes.ts
@@ -1,4 +1,5 @@
 import * as route11 from "./depth1-route1";
+import * as route21 from "./depth2-route1";
 import * as route81 from "./depth8-route1";
 import * as route82 from "./depth8-route2";
 import * as route101 from "./depth10-route1";
@@ -13,11 +14,11 @@ export const routes: Record<string, {
 }> = {
   "Default": defaultRoute,
   "Deeps 1 – Route 1": route11,
+  "Deeps 2 – Route 1": route21,
   "Deeps 8 – Route 1": route81,
   "Deeps 8 – Route 2": route82,
   "Deeps 10 – Route 1": route101,
   "Deeps 10 – Route 2": route102,
 };
-
 
 export type RouteKey = keyof typeof routes;
